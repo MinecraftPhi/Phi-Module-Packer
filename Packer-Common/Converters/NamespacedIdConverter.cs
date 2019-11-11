5bf4f1bc2a9919eb6d8727a18186a223a744069d
vdvman1 <vdvman88@gmail.com>
2019-11-11 23:01:38 +1100

    Load simple module information
    Validates namespaced ID format by (ab)using a JsonConverter

---
+++ Packer-Common/Converters/NamespacedIdConverter.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Phi.Packer.Common.Converters
+{
+    public class NamespacedIdConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(string);
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.String)
+            {
+                throw CreateException($"Expected string value, but found {reader.TokenType}.", reader);
+            }
+            else if(reader.Value is string id && !string.IsNullOrEmpty(id))
+            {
+                return IdRegex.IsMatch(id) ? id : throw CreateException($"Namespaced ID must match {IdRegex}", reader);
+            }
+            else
+            {
+                throw CreateException("Non-empty namespaced ID required.", reader);
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) => throw new NotImplementedException();
+
+        private static readonly Regex IdRegex = new Regex(@"\A[a-z0-9._-]+:[a-z0-9._-]+\z");
+
+        private static Exception CreateException(string message, JsonReader reader)
+        {
+            var info = (IJsonLineInfo)reader;
+            return new JsonSerializationException(message, reader.Path, info.LineNumber, info.LinePosition, null);
+        }
+    }
+}
---
+++ Packer-Common/Module.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using NuGet.Protocol;
+using NuGet.Versioning;
+using Phi.Packer.Common.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Phi.Packer.Common
+{
+    public class Module
+    {
+        [JsonProperty(Required = Required.Always)]
+        [JsonConverter(typeof(NamespacedIdConverter))]
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public SemanticVersion? Version { get; set; }
+
+        public static readonly string ModuleFileName = "module.json";
+        public static Module? Load(string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+
+            // If path points either to a directory or a file that isn't a module file
+            if (Path.GetFileName(path) is string fileName && fileName != ModuleFileName)
+            {
+                // If path points to a file that isn't a module file
+                if(File.Exists(path))
+                {
+                    path = Path.GetDirectoryName(path)!; // When the path points to a file it is guaranteed to have a directory
+                }
+
+                // Find module file at same location
+                path = Path.Combine(path, ModuleFileName);
+            }
+
+            return JsonConvert.DeserializeObject<Module>(File.ReadAllText(path), new SemanticVersionConverter());
+        }
+    }
+}
