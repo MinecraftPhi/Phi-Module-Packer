953c7dafe3d3852c439d5e9383b4fd8d19309959
vdvman1 <vdvman88@gmail.com>
2019-11-16 13:08:23 +1100

    Load profile lists Indicate whether a preprocessor could possibly run

--- Packer-Common/PreprocessorConfig.cs
+++ Packer-Common/PreprocessorConfig.cs
@@ -24,23 +24,41 @@ namespace Phi.Packer.Common
         [DefaultValue(PreprocessorType.Invalid)]
         public virtual PreprocessorType Type => PreprocessorType.Invalid;
 
+        [JsonIgnore]
+        public bool CanRun { get; }
+
+        public List<string> Profiles { get; } = new List<string>();
+
         // Limit subclasses to this assembly
-        internal PreprocessorConfig() { }
+        internal PreprocessorConfig(bool canRun) {
+            CanRun = canRun;
+        }
+
+        internal PreprocessorConfig()
+        {
+            CanRun = false;
+        }
     }
 
     public sealed class VerifiedPreprocessor : PreprocessorConfig
     {
         public override PreprocessorType Type => PreprocessorType.Verified;
+
+        public VerifiedPreprocessor() : base(true) { }
     }
 
     public sealed class NugetPreprocessor : PreprocessorConfig
     {
         public override PreprocessorType Type => PreprocessorType.Nuget;
+
+        public NugetPreprocessor() : base(true) { }
     }
 
     public sealed class LocalPreprocessor : PreprocessorConfig
     {
         public override PreprocessorType Type => PreprocessorType.Local;
+
+        public LocalPreprocessor() : base(true) { }
     }
 
     public sealed class CustomPreprocessor : PreprocessorConfig
@@ -53,5 +71,7 @@ namespace Phi.Packer.Common
         public override PreprocessorType Type => PreprocessorType.Split;
 
         public IDictionary<string, PreprocessorConfig> Preprocessors { get; } = new Dictionary<string, PreprocessorConfig>();
+
+        public SplitPreprocessor() : base(true) { }
     }
 }
