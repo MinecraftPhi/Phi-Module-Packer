9e93071d9c26ddc7188179dfbb9e6ff515712fc6
vdvman1 <vdvman88@gmail.com>
2019-11-16 11:13:12 +1100

    Add As helper method for safe inline access to subtype

---
+++ Packer-Common/Helpers/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phi.Packer.Common.Helpers
+{
+    public static class Extensions
+    {
+        public static T? As<T>(this object obj)
+            where T : class => obj as T;
+    }
+}
